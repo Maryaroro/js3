ef7beaf87cc804512c1a98d382b0bdbbcf47e64d
Maryaroro <6808255@gmail.com>
2023-03-03 22:02:52 +0300

    fixed js code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let price1= document.querySelector('.price1');
-let price2 =document.querySelector('.price2');
-let price3 =document.querySelector('.price3');
-let price4 =document.querySelector('.price4');
+const price1= document.querySelector('.price1');
+const price2 =document.querySelector('.price2');
+const price3 =document.querySelector('.price3');
+const price4 =document.querySelector('.price4');
 
-let total= Number(price1.textContent)+Number(price2.textContent)+Number(price3.textContent)+Number(price4.textContent);
-let sum=document.querySelector('.sum')
+const total= Number(price1.textContent)+Number(price2.textContent)+Number(price3.textContent)+Number(price4.textContent);
+const sum=document.querySelector('.sum')
 sum.textContent=total;
 
 let button=document.querySelector('.button')
